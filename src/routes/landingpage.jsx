da45fab48879ced68148a69172ff6b4ecf4961cd
zwood011 <zwood011@gmail.com>
2024-06-06 22:18:55 -0400

    ReDeploy

--- src/routes/landingpage.jsx
+++ src/routes/landingpage.jsx
@@ -6,12 +6,6 @@ import { Helmet } from 'react-helmet-async';
 const LandingPage = () => {
     const date = new Date().getFullYear();
 
-    return(
-        <div>
-            <Howdy />
-        </div>
-    )
-
     return (
         <>
             <div className='landing-container'>
