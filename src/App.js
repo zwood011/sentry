43ea6dc11ce013206395dc3aaf32ec8dd58dd23a
zwood011 <zwood011@gmail.com>
2024-06-09 07:57:44 -0400

    Cleaned CSS
    This file very much needs to be organized and separated still

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import useFilters from './hooks/usefilters';
 const App = () => {
     //TODO: Fix Card Titles - Large titles get cut off
     //TODO: Fix background visual error, especially bad on mobile devices
+    //TODO: Re-add animation for Card-Data
     //TODO: Look into designing error.jsx/ErrorBundary.jsx/Loading.jsx/404.html further
     //TODO: Potentionally minimize loading by making a CDN
 
