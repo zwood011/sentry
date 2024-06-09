c1346169454c55b82db9a8945cbd799038a566dd
zwood011 <zwood011@gmail.com>
2024-06-09 17:05:36 -0400

    Webpack Configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const TerserPlugin = require('terser-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: __dirname + '/dist',
-        publicPath: '/public',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
     },
     module: {
         rules: [
@@ -25,15 +25,21 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
                 test: /\.(png|jpe?g|gif)$/i,
-                type: 'asset/resource',
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                        },
+                    },
+                ],
             },
         ],
     },
-
     resolve: {
         extensions: ['.js', '.jsx'],
     },
@@ -51,12 +57,13 @@ module.exports = {
         ],
     },
     plugins: [
-        new CopyWebpackPlugin({
-            patterns: [{ from: 'public', to: '.' }],
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html',
+            inject: 'body',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
         }),
     ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        publicPath: '/',
-    },    
 };
