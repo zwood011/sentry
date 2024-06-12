e4a11c9b367cd6e8e318406796c15d05132bb50a
zwood011 <zwood011@gmail.com>
2024-06-11 20:39:44 -0400

    Lazy loading & suspense Implemented

--- src/index.js
+++ src/index.js
@@ -1,42 +1,38 @@
-/**
- * The main entry point for the React application.
- *
- * This file sets up the React Router and renders the application with the necessary providers and error handling.
- *
- * The main routes are defined in the `routes` array, which maps paths to their corresponding components.
- *
- * The `AppRouter` component wraps the routes and renders them within the `Router` component from `react-router-dom`.
- *
- * The application is then rendered using `createRoot` from `react-dom/client`, with the `HelmetProvider` and `ErrorBoundary` components providing additional functionality.
- */
-
-import React from 'react';
+import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
-import LandingPage from './routes/landingpage';
-import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
 
 const routes = [
     {
         path: '/',
-        element: <LandingPage />,
+        element: import('./routes/landingpage'),
     },
     {
         path: 'sentry',
-        element: <App />,
+        element: React.lazy(() => import('./App')),
     },
 ];
 
+/* The primary purpose for Suspense is to suspend the rendering of a component tree until some
+ condition is met, like lazy loading or data fetching. When a component wrapped in Suspense,
+ React tries to render and encounters a lazy-loaded component that
+ hasn't loaded yet. React will "suspend" the rendering of that component and its subtree." */
 const AppRouter = () => (
     <Router>
-        <Routes>
-            {routes.map(({ path, element }) => (
-                <Route key={path} path={path} element={element} />
-            ))}
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+            <HelmetProvider>
+                <ErrorBoundary>
+                    <Routes>
+                        {routes.map(({ path, element }) => (
+                            <Route key={path} path={path} element={element} />
+                        ))}
+                    </Routes>
+                </ErrorBoundary>
+            </HelmetProvider>
+        </Suspense>
     </Router>
 );
 
