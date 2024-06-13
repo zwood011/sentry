07c70647757f5812014fd219b96448c716dddd8d
zwood011 <zwood011@gmail.com>
2024-06-13 17:44:32 -0400

    Formatting

--- src/App.js
+++ src/App.js
@@ -17,126 +17,126 @@ import CardHandler from './components/card/cardhandler';
 const Filters = React.lazy(() => import('./components/filters'));
 
 const App = () => {
-    //TODO: Fix background visual error, remove background from the body tag
-    //TODO: Look into designing error.jsx/ErrorBundary.jsx/Loading.jsx/404.html further
-    //TODO: Potentionally minimize loading by making a CDN
+  //TODO: Fix background visual error, remove background from the body tag
+  //TODO: Look into designing error.jsx/ErrorBundary.jsx/Loading.jsx/404.html further
+  //TODO: Potentionally minimize loading by making a CDN
 
-    const [loading, setLoading] = useState(true);
-    const [errorMessage, setErrorMessage] = useState(null);
-    const [objects, setObjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [objects, setObjects] = useState([]);
 
-    const date = new Date().getFullYear();
+  const date = new Date().getFullYear();
 
-    const fetchData = useCallback(() => {
-        setLoading(true);
-        axios
-            .get('https://neo-nasa.netlify.app/.netlify/functions/index')
-            .then((response) => {
-                const objects = response.data.data.map((obj) => ({
-                    fullname: obj.fullname,
-                    ps_cum: obj.ps_cum,
-                    des: obj.des,
-                    diameter: obj.diameter,
-                    h: obj.h,
-                    id: obj.id,
-                    ip: obj.ip,
-                    last_obs: obj.last_obs,
-                    last_obs_jd: obj.last_obs_jd,
-                    n_imp: obj.n_imp,
-                    ps_max: obj.ps_max,
-                    range: obj.range,
-                    ts_max: obj.ts_max,
-                    v_inf: obj.v_inf,
-                }));
-                setObjects(objects);
-                setLoading(false);
-            })
-            .catch((error) => {
-                setErrorMessage(error.message);
-                setLoading(false);
-            });
-    }, []);
+  const fetchData = useCallback(() => {
+    setLoading(true);
+    axios
+      .get('https://neo-nasa.netlify.app/.netlify/functions/index')
+      .then((response) => {
+        const objects = response.data.data.map((obj) => ({
+          fullname: obj.fullname,
+          ps_cum: obj.ps_cum,
+          des: obj.des,
+          diameter: obj.diameter,
+          h: obj.h,
+          id: obj.id,
+          ip: obj.ip,
+          last_obs: obj.last_obs,
+          last_obs_jd: obj.last_obs_jd,
+          n_imp: obj.n_imp,
+          ps_max: obj.ps_max,
+          range: obj.range,
+          ts_max: obj.ts_max,
+          v_inf: obj.v_inf,
+        }));
+        setObjects(objects);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+        setLoading(false);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, [fetchData]);
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
-    const { filteredObjects, onFilterName, onFilterSize, onFilterOldest, onFilterNewest, onClear } = useFilters(objects);
+  const { filteredObjects, onFilterName, onFilterSize, onFilterOldest, onFilterNewest, onClear } = useFilters(objects);
 
-    if (loading) return <Loading />;
+  if (loading) return <Loading />;
 
-    return (
-        <>
-            <Helmet>
-                <title>Card Fetcher | Sentry Grabber</title>
-                <meta
-                    name='description'
-                    data-rh='true'
-                    content='Explore comprehensive data related to Earth impact risk, close-approaches, and asteroid monitoring from Neo-Nasareal-time asteroid data, NASA asteroid data, user-friendly asteroid tracker interface, intuitive asteroid card format, asteroid warning updates.'
-                />
-            </Helmet>
+  return (
+    <>
+      <Helmet>
+        <title>Card Fetcher | Sentry Grabber</title>
+        <meta
+          name='description'
+          data-rh='true'
+          content='Explore comprehensive data related to Earth impact risk, close-approaches, and asteroid monitoring from Neo-Nasareal-time asteroid data, NASA asteroid data, user-friendly asteroid tracker interface, intuitive asteroid card format, asteroid warning updates.'
+        />
+      </Helmet>
 
-            <div className='App' role='main'>
-                {!loading && (
-                    <>
-                        <header className={headerAnimation}>
-                            <div className='app-header'>
-                                <nav>
-                                    <Filters
-                                        className='app-nav-item'
-                                        onFilterName={onFilterName}
-                                        onFilterSize={onFilterSize}
-                                        onFilterOldest={onFilterOldest}
-                                        onFilterNewest={onFilterNewest}
-                                        onClear={onClear}
-                                        aria-label='Filtering options for Earth impact data'
-                                    />
-                                </nav>
-                                <h1 className='app-nav-item'>Earth Impact Data</h1>
-                                <p className='app-nav-item'>
-                                    Explore comprehensive data brought to you by the{' '}
-                                    <Link
-                                        className='Hyperlink'
-                                        to='https://cneos.jpl.nasa.gov/sentry/'
-                                        aria-label='CNEOS impact monitoring system'>
-                                        CNEOS
-                                    </Link>{' '}
-                                    impact monitoring system
-                                </p>
-                            </div>
-                        </header>
+      <div className='App' role='main'>
+        {!loading && (
+          <>
+            <header className={headerAnimation}>
+              <div className='app-header'>
+                <nav>
+                  <Filters
+                    className='app-nav-item'
+                    onFilterName={onFilterName}
+                    onFilterSize={onFilterSize}
+                    onFilterOldest={onFilterOldest}
+                    onFilterNewest={onFilterNewest}
+                    onClear={onClear}
+                    aria-label='Filtering options for Earth impact data'
+                  />
+                </nav>
+                <h1 className='app-nav-item'>Earth Impact Data</h1>
+                <p className='app-nav-item'>
+                  Explore comprehensive data brought to you by the{' '}
+                  <Link
+                    className='Hyperlink'
+                    to='https://cneos.jpl.nasa.gov/sentry/'
+                    aria-label='CNEOS impact monitoring system'>
+                    CNEOS
+                  </Link>{' '}
+                  impact monitoring system
+                </p>
+              </div>
+            </header>
 
-                        <main className={mainAnimation}>
-                            <CardHandler
-                                isLoading={loading}
-                                errorMessage={errorMessage}
-                                objects={filteredObjects}
-                                retryFetch={fetchData}
-                                aria-live='polite'
-                                aria-relevant='additions removals'
-                            />
-                        </main>
+            <main className={mainAnimation}>
+              <CardHandler
+                isLoading={loading}
+                errorMessage={errorMessage}
+                objects={filteredObjects}
+                retryFetch={fetchData}
+                aria-live='polite'
+                aria-relevant='additions removals'
+              />
+            </main>
 
-                        <footer className='Footer-Sentry'>
-                            <p className='footerText'>© {date} Zachary Wood. All rights reserved.</p>
-                            <div className='Page-Description' aria-label='Page Description'>
-                                <p className='Description-Text'>
-                                    Powered by{' '}
-                                    <Link
-                                        className='Hyperlink'
-                                        to='https://cneos.jpl.nasa.gov/sentry/'
-                                        aria-label='CNEOS impact monitoring system'>
-                                        CNEOS
-                                    </Link>{' '}
-                                    impact monitoring system
-                                </p>
-                            </div>
-                        </footer>
-                    </>
-                )}
-            </div>
-        </>
-    );
+            <footer className='Footer-Sentry'>
+              <p className='footerText'>© {date} Zachary Wood. All rights reserved.</p>
+              <div className='Page-Description' aria-label='Page Description'>
+                <p className='Description-Text'>
+                  Powered by{' '}
+                  <Link
+                    className='Hyperlink'
+                    to='https://cneos.jpl.nasa.gov/sentry/'
+                    aria-label='CNEOS impact monitoring system'>
+                    CNEOS
+                  </Link>{' '}
+                  impact monitoring system
+                </p>
+              </div>
+            </footer>
+          </>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default App;
