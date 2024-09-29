43346ee76eda9fde91bcd65294f404b14d45de56
Zach Wood <zwood011@gmail.com>
2024-09-29 16:14:16 -0400

    Added filtering functions to a singular object

--- src/hooks/usefilters.js
+++ src/hooks/usefilters.js
@@ -1,8 +1,3 @@
-/* 
-  See a visualization of the data flow at 
-  https://sentrygrabber.netlify.app/dataflow.png☺
-*/
-
 import { useState, useEffect } from 'react';
 
 const useFilters = (initialObjects) => {
@@ -14,37 +9,35 @@ const useFilters = (initialObjects) => {
         setFilteredObjects(initialObjects);
     }, [initialObjects]);
 
-    const onFilterName = (newFilter) => {
-        const filtered = objects.filter((obj) => obj.fullname.toLowerCase().includes(newFilter.toLowerCase()));
-        setFilteredObjects(filtered);
-    };
+    const filters = {
+        onFilterName: (newFilter) => {
+            const filtered = objects.filter((obj) => obj.fullname.toLowerCase().includes(newFilter.toLowerCase()));
+            setFilteredObjects(filtered);
+        },
 
-    const onClear = () => {
-        setFilteredObjects(objects);
-    };
+        onClear: () => {
+            setFilteredObjects(objects);
+        },
 
-    const onFilterSize = () => {
-        const filtered = [...filteredObjects].sort((a, b) => b.diameter - a.diameter);
-        setFilteredObjects(filtered);
-    };
+        onFilterSize: () => {
+            const filtered = [...filteredObjects].sort((a, b) => b.diameter - a.diameter);
+            setFilteredObjects(filtered);
+        },
 
-    const onFilterOldest = () => {
-        const filtered = [...filteredObjects].sort((a, b) => a.last_obs_jd - b.last_obs_jd);
-        setFilteredObjects(filtered);
-    };
+        onFilterOldest: () => {
+            const filtered = [...filteredObjects].sort((a, b) => a.last_obs_jd - b.last_obs_jd);
+            setFilteredObjects(filtered);
+        },
 
-    const onFilterNewest = () => {
-        const filtered = [...filteredObjects].sort((a, b) => b.last_obs_jd - a.last_obs_jd);
-        setFilteredObjects(filtered);
+        onFilterNewest: () => {
+            const filtered = [...filteredObjects].sort((a, b) => b.last_obs_jd - a.last_obs_jd);
+            setFilteredObjects(filtered);
+        },
     };
 
     return {
         filteredObjects,
-        onFilterName,
-        onFilterSize,
-        onFilterOldest,
-        onFilterNewest,
-        onClear,
+        filters
     };
 };
 
