30b492a1e0d5d13cc72b20a23ac5a22ae86d9e06
Zach Wood <zwood011@gmail.com>
2024-10-20 21:47:45 -0400

    Temporary prod push for Bing's Indexnow API

--- src/index.js
+++ src/index.js
@@ -11,22 +11,44 @@ import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import { Provider } from 'react-redux';
 
+import { Provider } from 'react-redux';
 import store from './redux/store.js';
 
 import ErrorBoundary from './components/ErrorBoundary';
 import LandingPage from '../src/landingpage';
-
 import Sentry from '../src/routes/Sentry.jsx';
 import Error404 from './routes/Error404';
 
 const App = () => {
     useEffect(() => {
+        // remove the noscript element if it exists
         const noscriptElement = document.querySelector('noscript');
         if (noscriptElement) {
             noscriptElement.remove();
         }
+
+        // make the IndexNow request
+        const indexNowData = {
+            host: "sentrygrabber.netlify.app",
+            key: "489094d643c948c1aac3ff5cb9b981e2",
+            keyLocation: "https://sentrygrabber.netlify.app/489094d643c948c1aac3ff5cb9b981e2.txt",
+            urlList: [
+                "https://sentrygrabber.netlify.app/",
+                "https://sentrygrabber.netlify.app/sentry/"
+            ]
+        };
+
+        fetch("https://api.indexnow.org/IndexNow", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify(indexNowData)
+        })
+            .then(response => response.json())
+            .then(data => console.log('indexnow response:', data))
+            .catch(error => console.error('error:', error));
     }, []);
 
     return (
