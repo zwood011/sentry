3066552a87af7a9d62b6864ea5d5e3c1c9988992
Zach Wood <zwood011@gmail.com>
2024-10-21 01:29:00 -0400

    Update index.js

--- netlify/functions/index.js/index.js
+++ netlify/functions/index.js/index.js
@@ -5,6 +5,7 @@ const API_URL = process.env.NASA_API_URL;
 exports.handler = async () => {
     try {
         const response = await axios.get(API_URL);
+        console.log(`yeet ${response}`)
         return {
             statusCode: 200,
             body: JSON.stringify(response.data),
