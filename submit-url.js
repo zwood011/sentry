4d0940aa03a1bfa07c5ce2da0cf6e9c6b32fe701
Zach Wood <zwood011@gmail.com>
2024-10-21 00:46:07 -0400

    Added a toggle for the automatic http request during build

--- submit-url.js
+++ submit-url.js
@@ -1,25 +1,32 @@
-// submit-url.js
 const axios = require('axios');
 
-const requestData = {
-    siteUrl: "https://sentrygrabber.netlify.app",
-    urlList: [
-        "https://sentrygrabber.netlify.app",
-        "https://sentrygrabber.netlify.app/sentry",
-    ],
-};
+exports.handler = async () => {
+    const submitUrls = process.env.SUBMIT_URL === 'true'; // reference value for the toggle in netlify.toml
 
-async function submitUrl() {
-    try {
+    if (submitUrls) {
+        const requestData = {
+            siteUrl: "https://sentrygrabber.netlify.app",
+            urlList: [
+                "https://sentrygrabber.netlify.app",
+                "https://sentrygrabber.netlify.app/sentry",
+            ],
+        };
+
         await axios.post(`https://ssl.bing.com/webmaster/api.svc/json/SubmitUrlBatch?apikey=32c8d1d8c05f4e1d85b7173019fbb751`, requestData, {
             headers: {
                 'Content-Type': 'application/json; charset=utf-8',
             },
         });
-        console.log('URLs submitted successfully');
-    } catch (error) {
-        console.error('Error submitting URLs:', error);
-    }
-}
 
-submitUrl();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'URLs submitted successfully' }),
+        };
+    } else {
+        console.log("URL submission skipped");
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'URL submission skipped' }),
+        };
+    }
+};
