425b23adf57042fe96d6375d1f74b27ee8b74a0a
Zach Wood <zwood011@gmail.com>
2024-10-20 23:57:19 -0400

    Changed to XML

--- netlify/functions/submit-url.js
+++ netlify/functions/submit-url.js
@@ -1,16 +1,22 @@
 const axios = require('axios');
 
 exports.handler = async (event) => {
-    const requestData = {
-        siteUrl: "https://sentrygrabber.netlify.app",
-        urlList: [
-            "https://sentrygrabber.netlify.app",
-            "https://sentrygrabber.netlify.app/sentry",
-        ]
-    };
+    const requestData = `
+    <SubmitUrlBatch xmlns="http://schemas.datacontract.org/2004/07/Microsoft.Bing.Webmaster.Api">
+        <siteUrl>https://sentrygrabber.netlify.app</siteUrl>
+        <urlList>
+            <string xmlns="http://schemas.microsoft.com/2003/10/Serialization/Arrays">https://sentrygrabber.netlify.app</string>
+            <string xmlns="http://schemas.microsoft.com/2003/10/Serialization/Arrays">https://sentrygrabber.netlify.app/sentry</string>
+        </urlList>
+    </SubmitUrlBatch>
+    `;
 
     try {
-        const response = await axios.post(`https://ssl.bing.com/webmaster/api.svc/json/SubmitUrlbatch?apikey=32c8d1d8c05f4e1d85b7173019fbb751`, requestData);
+        const response = await axios.post(`https://ssl.bing.com/webmaster/api.svc/xml/SubmitUrlBatch?apikey=32c8d1d8c05f4e1d85b7173019fbb751`, requestData, {
+            headers: {
+                'Content-Type': 'application/xml; charset=utf-8',
+            },
+        });
         return {
             statusCode: 200,
             body: JSON.stringify(response.data),
@@ -18,7 +24,7 @@ exports.handler = async (event) => {
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Error submitting URL batch.' }),
+            body: JSON.stringify({ error: 'error submitting url batch.' }),
         };
     }
 };
