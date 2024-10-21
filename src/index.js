35df8954bfc00d00ee37b5c8d4ef0bca4625e1e3
Zach Wood <zwood011@gmail.com>
2024-10-20 23:00:45 -0400

    Reverted IndexNow changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
+/*
+!       Add some more clarity in the Cards, show what the data means
+
+TODO:   Complete and design the question mark button (?) that renders a popup
+*       ^ Clarify filters, make it useful
+
+*       The large asteroid name still has irregular sizing
+*/
+
 import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
+
 import store from './redux/store.js';
+
 import ErrorBoundary from './components/ErrorBoundary';
 import LandingPage from '../src/landingpage';
+
 import Sentry from '../src/routes/Sentry.jsx';
 import Error404 from './routes/Error404';
-import axios from 'axios'; // import axios here
 
 const App = () => {
     useEffect(() => {
@@ -16,32 +27,7 @@ const App = () => {
         if (noscriptElement) {
             noscriptElement.remove();
         }
-
-        const indexNowData = {
-            host: "sentrygrabber.netlify.app",
-            key: "489094d643c948c1aac3ff5cb9b981e2",
-            keyLocation: "https://sentrygrabber.netlify.app/489094d643c948c1aac3ff5cb9b981e2.txt",
-            urlList: [
-                "https://sentrygrabber.netlify.app/",
-                "https://sentrygrabber.netlify.app/sentry/"
-            ]
-        };
-
-        console.log('sending data to IndexNow...');
-
-        axios.post("https://cors-anywhere.herokuapp.com/https://api.indexnow.org/IndexNow", indexNowData, {
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-            },
-        })
-        .then(response => {
-            console.log('Response data:', response.data); // log the response
-        })
-        .catch(error => {
-            console.error('Error:', error.response ? error.response.data : error.message);
-        });
-
-    }, []); // Added missing dependency array for useEffect
+    }, []);
 
     return (
         <Router>
