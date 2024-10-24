82e3a44c1c7fd42f8582ce5da9c10d2125700d1b
Zach Wood <zwood011@gmail.com>
2024-10-23 23:16:16 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,6 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getPerformance } from "firebase/performance";
 
-
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
