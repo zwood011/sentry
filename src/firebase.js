9d67df170d3d7055c9fde10cd62e60dd047fe80b
Zach Wood <zwood011@gmail.com>
2024-10-23 23:10:15 -0400

    Test build

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,24 @@
 // src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getPerformance } from "firebase/performance";
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCGFKpSOEzUFCicg0KN4Mwoydnhzy_TG_o",
-    authDomain: "sentry-grabber.firebaseapp.com",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: "sentry-grabber",
     storageBucket: "sentry-grabber.appspot.com",
     messagingSenderId: "577550175553",
-    appId: "1:577550175553:web:84da6ccab5c2f3fa10e5c0",
+    appId: process.env.REACT_APP_APP_ID,
     measurementId: "G-RGGE69VMC1"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// eslint-disable-next-line no-undef
+const perf = getPerformance(app); // eslint-disable-line no-unused-vars
 
 // Initialize Firestore
 const db = getFirestore(app);
