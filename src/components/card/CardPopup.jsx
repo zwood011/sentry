16f9ff4eea52e0003e7b75d7b09bb250f686950f
Zach Wood <zwood011@gmail.com>
2024-11-15 03:56:10 -0500

    Added ps_cum details

--- src/components/card/CardPopup.jsx
+++ src/components/card/CardPopup.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-
 const CardPopup = ({ obj }) => {
+    const getRiskMessage = (ps_cum) => {
+        if (ps_cum < -2) {
+            return "Minimal risk. No significant concerns";
+        } else if (ps_cum >= -2 && ps_cum < 0) {
+            return "Low to moderate risk. Monitoring is recommended";
+        } else if (ps_cum >= 0) {
+            return "Elevated risk. Immediate analysis required";
+        } else {
+            return "Risk level unknown.";
+        }
+    };
 
     return (
         <div className="Card-Popup">
-            {/*<strong> Show unit by using: </strong> {obj.diameter} km<br />*/}
-            <p className="text-light">Currently developing these panels now!</p>
+            <div className="Popup-Details">
+                <p className="text-light">
+                    Selected asteroid CHR: <strong>{obj.ps_cum}</strong> <br />
+                    <span>{getRiskMessage(obj.ps_cum)}</span>
+                </p>
+                <p className="Ps_cum-Details">
+                    The cumulative hazard rating refers to a measure that assesses the overall risk posed by a particular asteroid over time.
+                </p>
+            </div>
         </div>
-    )
+    );
 };
 
 export default CardPopup;
